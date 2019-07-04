a3a29ad3f35afcd37cc0858a54e47a7c68dacad1
Melissa de Miranda Teodoro Soares <melissa.soares@spread.com.br>
2019-07-04 15:43:47 -0300

    Configuração do Server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+'use strict' // Faz uma análise criteriosa no arquivo, de acordo com a sintaxe
+ 
+const http = require('http') //Servidor HTTP
+const express = require('express') //Express - modulo do Node para criar um MVC Model e outros recursos
+const debug = require('debug')('nodestr:server') //Debugar a aplicação
+
+/*const xpto = require('./teste') // Indica que o caminho vem de uma pasta específica  e nao do Node Modules*/
+
