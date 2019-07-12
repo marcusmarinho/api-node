5aaa358227a80c13ff55fa33d5ab1c7f2bad40cc
Melissa de Miranda Teodoro Soares <melissa.soares@spread.com.br>
2019-07-12 15:28:26 -0300

    Criação das rotas, controller e repository do Customer.

--- src/models/customer.js
+++ src/models/customer.js
@@ -3,18 +3,18 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const schema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
 module.exports = mongoose.model('Customer', schema);
--- src/models/order.js
+++ src/models/order.js
@@ -3,40 +3,40 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const schema = new Schema({
-    customer: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Customer'
+  customer: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Customer',
+  },
+  number: {
+    type: String,
+    required: true,
+  },
+  createDate: {
+    type: Date,
+    require: true,
+    default: Date.now,
+  },
+  status: {
+    type: String,
+    required: true,
+    enum: ['created', 'done'], // Apenas poderá receber esses dois valores.
+    default: 'created',
+  },
+  items: [{
+    quantity: {
+      type: Number,
+      required: true,
+      default: 1,
     },
-    number: {
-        type: String,
-        required: true
+    price: {
+      type: Number,
+      required: true,
     },
-    createDate: {
-        type: Date,
-        require: true,
-        default: Date.now
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product',
     },
-    status: {
-        type: String,
-        required: true,
-        enum: ['created', 'done'], //Apenas poderá receber esses dois valores.
-        default: 'created'
-    },
-    items: [{
-        quantity: {
-            type: number,
-            required: true,
-            default: 1
-        },
-        price: {
-            type: number,
-            required: true
-        },
-        product: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Product'
-        }
-    }]
+  }],
 });
 
 module.exports = mongoose.model('Order', schema);
